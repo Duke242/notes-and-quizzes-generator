ce2c24e58bd02b88dd7be0cf716291ae1ce24443
Duke242 <innominate3301@proton.me>
2024-01-16 09:27:07 -0500

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,22 @@
-import { Inter } from "next/font/google";
-import PlausibleProvider from "next-plausible";
-import { getSEOTags } from "@/libs/seo";
-import ClientLayout from "@/components/LayoutClient";
-import config from "@/config";
-import "./globals.css";
+import { Inter } from "next/font/google"
+import PlausibleProvider from "next-plausible"
+import { getSEOTags } from "@/libs/seo"
+import ClientLayout from "@/components/LayoutClient"
+import config from "@/config"
+import "./globals.css"
 
-const font = Inter({ subsets: ["latin"] });
+const font = Inter({ subsets: ["latin"] })
 
 export const viewport = {
   // Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
   themeColor: config.colors.main,
   width: "device-width",
   initialScale: 1,
-};
+}
 
 // This adds default SEO tags to all pages in our app.
 // You can override them in each page passing params to getSOTags() function.
-export const metadata = getSEOTags();
+export const metadata = getSEOTags()
 
 export default function RootLayout({ children }) {
   return (
@@ -29,7 +29,8 @@ export default function RootLayout({ children }) {
       <body>
         {/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
         <ClientLayout>{children}</ClientLayout>
+        <Analytics />
       </body>
     </html>
-  );
+  )
 }
