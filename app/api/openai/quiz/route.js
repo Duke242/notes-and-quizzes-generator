b24e32ed887c1f0d0a7555d063a3266496cfd20a
Duke242 <innominate3301@proton.me>
2024-02-11 20:39:20 -0500

    updated the quiz that is given

--- app/api/openai/quiz/route.js
+++ app/api/openai/quiz/route.js
@@ -15,16 +15,16 @@ export async function POST(req) {
       messages: [
         {
           role: "system",
-          content: `You are an excellent teacher. Give me a quiz based on the title and notes, using 3 questions and do not use more than 500 words. And use short answer questions. Single space the answer. Do not give the answers. Do not restate the notes, just give the quiz questions.`,
+          content: `You are an excellent teacher. This is what I know about ${payload.title}, ${payload.explanation}. Give me a short quiz to reinforce my learning. Only return the questions and nothing else."
+          `,
         },
         {
           role: "user",
           content: `Title: ${payload.title} and content: ${payload.explanation}`,
         },
       ],
-      temperature: 0.7,
-      max_tokens: 1000,
-      top_p: 1,
+      temperature: 0,
+      max_tokens: 3500,
     })
     return NextResponse.json({ response }, { status: 201 })
   } catch (error) {
