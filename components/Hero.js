4916a2dc5bf1bfb2ca07e2d5d32cad95b6ef3572
Duke242 <innominate3301@proton.me>
2024-02-17 16:43:56 -0500

    changed the copy in hero

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className="p-4 md:p-8 lg:p-12">
       <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl text-center text-glacierBlue mt-4 md:mt-8 lg:mt-12">
-        Building a learning tool that fits your brain's way of learning.
+        A learning tool that fits your brain's way of learning.
       </h1>
       <p className="text-center text-gray-500 mt-4 md:mt-6 lg:mt-8 text-lg md:text-2xl">
         Learn better by absorbing, not forcing.
