d1ce8871d350501f46986fc0f35a2ace0a3bd60d
Duke242 <innominate3301@proton.me>
2024-03-28 13:52:02 -0400

    changed the copy

--- components/Hero.js
+++ components/Hero.js
@@ -6,11 +6,12 @@ import VideoComponent from "./VideoComponent"
 const Hero = () => {
   return (
     <section className="p-4 md:p-8 lg:p-12">
-      <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl text-center text-glacierBlue mt-4 md:mt-8 lg:mt-12">
-        A learning tool that fits your brain's way of learning.
+      <h1 className="font-bold text-3xl md:text-4xl lg:text-4xl text-center text-glacierBlue mt-4 md:mt-8 lg:mt-12">
+        Stay ahead with our automated note-taking tool. Simply input the topic,
+        receive the notes, and reinforce learning effortlessly.
       </h1>
       <p className="text-center text-gray-500 mt-4 md:mt-6 lg:mt-8 text-lg md:text-2xl">
-        Learn better by absorbing, not forcing.
+        Whether learning solo or in a group, our tool enhances your experience.
       </p>
       <div className="w-full text-center mt-6 md:mt-8 lg:mt-10">
         <ButtonSignin
